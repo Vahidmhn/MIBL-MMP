bea4cbbe4c1854d69e0d84a704cb53428813247d
Vahidmhn <36528251+Vahidmhn@users.noreply.github.com>
2021-04-24 02:45:37 -0400

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,34 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=
+CND_ARTIFACT_NAME_Debug=cplus4
+CND_ARTIFACT_PATH_Debug=cplus4
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=lastversion.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/lastversion.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=
+CND_ARTIFACT_NAME_Release=cplus4
+CND_ARTIFACT_PATH_Release=cplus4
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=lastversion.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/lastversion.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
